2d0ab912137108677f7e1af5c9066075fd1226d8
limcod <582373957@qq.com>
2020-10-08 18:44:53 +0800

    2020.10.08 晚上

--- node.js/students/app.js
+++ node.js/students/app.js
@@ -1,6 +1,21 @@
 // 引入http模块
 const http = require('http');
+// 引入router模块
+const getRouter = require('router');
+// 获取路由对象
+const router = getRouter();
+
+router.get('/test',(req,res) => {
+    res.end('test')
+})
+
+router.get('/index',(req,res) => {
+    
+})
+
+// 数据库连接
 require('./model/connect.js');
+// 学生信息集合
 const Student = require('./model/user.js')
 // 创建网站服务器
 const app = http.createServer();
