0b45e8a3572dc788eeb02cfaa64044ab32b86f4e
limcod <582373957@qq.com>
2020-10-23 14:48:36 +0800

    2020.10.23

--- node.js/students/app.js
+++ node.js/students/app.js
@@ -3,13 +3,26 @@ const http = require('http');
 // 引入router模块
 const getRouter = require('router');
 // 引入模板引擎
-// const template = require('art-template');
+const template = require('art-template');
+// 引入path模块
+const path = require('path');
+
+// 引入静态资源访问模块
+const serveStatic = require('serve-static');
+
 // 获取路由对象
 const router = getRouter();
 
+// 实现静态资源访问服务
+const serve = serveStatic(path.join(__dirname),'public')
+
+// 配置模板的根目录
+template.defaults.root = path.join(__dirname,'views'); 
+
 // 呈递学生档案信息页面
-router.get('/test',(req,res) => {
-    res.end('test')
+router.get('/add',(req,res) => {
+    let html = res.end('index.art', {})
+    res.end(html);
 })
 
 // 呈递学生档案信息列表页面
@@ -25,9 +38,8 @@ const Student = require('./model/user.js')
 const app = http.createServer();
 // 当客户端访问服务器端的时候
 app.on('request',(req,res) => {
-    router(req, res, () => {
-        console.log('1');
-    })
+    router(req, res, () => {})
+    serve(req,res,() => {})
 })
 
 app.listen(80);
